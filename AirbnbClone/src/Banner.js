7b4fcd62af2cb240dd76f7507b3456747d302eb2
hernan verar <hernanverar@gmail.com>
2023-09-05 23:06:24 -0700

    This commit updates our banner inside our image to make it look clean

--- AirbnbClone/src/Banner.js
+++ AirbnbClone/src/Banner.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import './Banner.css'
+import React from 'react';
+import './Banner.css';
 import { Button } from '@mui/material';
+import cabin from './images/cabin.jpeg';
 
 function Banner() {
   return (
-    <div className='banner'>
+    <div className="banner">
+      <div className='banner__image-container'>
+        <img className="banner__image" src={cabin} alt="Cabin" />
+        <div className='banner__info'>
+          <h1>Welcome, this is our Banner. You can be creative with this!</h1>
+          <Button variant='outlined'>Explore in this City</Button>
+        </div>
+      </div>
+
       <div className='banner_search'>
         <Button 
-        className='banner__searchButton'
-        variant='outlined'>Search Dates</Button>
+          className='banner__searchButton'
+          variant='outlined'>
+          Search Dates
+        </Button>
       </div>
-      
-      <div className='banner__info'>
-        <h1>Welcome this is our Banner you can be creative with this!</h1>
-        <Button variant='outlined'>explore in this City</Button>
-      </div>
-    </div> 
-  )
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
+
