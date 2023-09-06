a49c05e81c2021c31fb17aa1cedc121501a061e6
hernan verar <hernanverar@gmail.com>
2023-09-06 13:37:51 -0700

    This commit adds search files to our button in our page WIP on adding css

--- AirbnbClone/src/Banner.js
+++ AirbnbClone/src/Banner.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import './Banner.css';
-import { Button } from '@mui/material';
-import cabin from './images/cabin.jpeg';
+import React, { useState } from "react";
+import "./Banner.css";
+import { Button } from "@mui/material";
+import cabin from "./images/cabin.jpeg";
+import Search from "./Search";
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner">
-      <div className='banner__image-container'>
+      <div className="banner__image-container">
         <img className="banner__image" src={cabin} alt="Cabin" />
-        <div className='banner__info'>
+        <div className="banner__info">
           <h1>Welcome, this is our Banner. You can be creative with this!</h1>
-          <Button variant='outlined'>Explore in this City</Button>
+          <Button variant="outlined">Explore this City</Button>
         </div>
       </div>
 
-      <div className='banner_search'>
-        <Button 
-          className='banner__searchButton'
-          variant='outlined'>
+      <div className="banner_search">
+        {showSearch && <Search />}
+
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
           Search Dates
         </Button>
       </div>
@@ -26,4 +33,3 @@ function Banner() {
 }
 
 export default Banner;
-
