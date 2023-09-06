8f7f898cc6c916627ea72525e53fdedba0b7d434
hernan verar <hernanverar@gmail.com>
2023-09-05 17:21:30 -0700

    This commit add logo to our page and css to make our logo and icons be in the rightr place

--- vite/src/Header.js
+++ vite/src/Header.js
@@ -1,11 +1,25 @@
 import React from "react";
+import "./Header.css";
+import logo from "./images/logo.jpeg";
+import LanguageIcon from "@mui/icons-material/Language";
+import SearchIcon from "@mui/icons-material/Search";
 
-function Header(){
-    return (
-        <React.Fragment>
-            <h1>Airbnb App</h1>
-        </React.Fragment>
-    );
+function Header() {
+  return (
+    <div className="header">
+      <img className="header__icon" src={logo} />
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+        <LanguageIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Book a Room</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
